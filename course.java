14edd5dc5096e9006bbc58581dc40794217c8a30
Zachary Alberico <alberz@rpi.edu>
2010-02-23 15:58:32 -0500

    course.java added

---
+++ course.java
@@ -0,0 +1,17 @@
+public class Course{
+    public Course() {
+	System.out.println("Welcome to Academic-Toolkit");
+    }
+    public void addTest(Test test){
+    }
+    public void addQuiz(Quiz quiz){
+    }
+    public void addHw(Hw hw){
+    }
+    public void gradeCalc(){
+    }
+
+    public static void main(String[] arguments){
+    }
+}
+    
